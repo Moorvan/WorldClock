936b2bfea9bfb82dbeac3655c622bca533a3cd84
mufanx <35735090+mufanx@users.noreply.github.com>
2019-09-25 09:15:18 +0800

    ClockTest
    测试时钟类，对时钟类的实例化，时间赋值和读取的操作进行测试，没有出现失效。

--- test/stu/homework/WorldClock/ClockTest.java
+++ test/stu/homework/WorldClock/ClockTest.java
@@ -1,7 +1,40 @@
 package stu.homework.WorldClock;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class ClockTest {
+    static Clock[] clocks = new Clock[5];
+
+    @Test
+    @BeforeAll
+    static void init() {
+        clocks[0] = new Clock("Beijing", -8);
+        clocks[1] = new Clock("London", 0);
+        clocks[2] = new Clock("Moscow", -4);
+        clocks[3] = new Clock("Sydney", -10);
+        clocks[4] = new Clock("New York", 5);
+    }
+
+    @Test
+    @DisplayName("set and get the Clock time")
+    void clockSetGet() {
+        clocks[0].setLocalTime(LocalTime.of(1, 10));
+        clocks[1].setLocalTime(LocalTime.of(3, 20));
+        clocks[2].setLocalTime(LocalTime.of(4, 10));
+        assertAll(
+                ()->assertEquals("Beijing(offset:-8): 01:10", clocks[0].getLocation() + "(offset:" + clocks[0].getUTCOffSet() + "): " + clocks[0].getLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"))),
+                ()->assertEquals("London(offset:0): 03:20", clocks[1].getLocation() + "(offset:" + clocks[1].getUTCOffSet() + "): " + clocks[1].getLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"))),
+                ()->assertEquals("Moscow(offset:-4): 04:10", clocks[2].getLocation() + "(offset:" + clocks[2].getUTCOffSet() + "): " + clocks[2].getLocalTime().format(DateTimeFormatter.ofPattern("HH:mm")))
+                );
+    }
 
 }
